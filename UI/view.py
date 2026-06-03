2c19f7774f1a95e9825cd58039f6f6fc84d86274
GiuliaCampini <giulia.campini@gmail.com>
2026-06-03 11:20:43 +0200

    Update view.py

--- UI/view.py
+++ UI/view.py
@@ -25,6 +25,9 @@ class View(ft.UserControl):
 
         self._ddAnno1 = ft.Dropdown(label="Da", hint_text="Anno")
         self._ddAnno2 = ft.Dropdown(label="A", hint_text="Anno")
+        self._controller.fillDDyears()
+        print(self._ddAnno1.value)
+        print(self._ddAnno2.value)
         self._btnCreaGrafo = ft.ElevatedButton(text="Crea Grafo", on_click=self._controller.handleCreaGrafo)
 
         cont1 = ft.Container(self._ddAnno1, width=250)
